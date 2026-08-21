215f8f4e9a17ad94bf070e3ae127a4dc5713a017
Gokulbala9 <25ucs167@kamarajengg.edu.in>
2026-08-21 14:58:14 +0530

    Update 3A.py

--- 3A.py
+++ 3A.py
@@ -32,13 +32,34 @@ class Stack:
     def display(self):
         print("Stack elements:", self.stack)
 
-s = Stack(3)
-s.push("Python Programming")
-s.push("Data Structures")
-s.push("Computer Networks")
-s.push("Operating Systems")
-s.display()
-print("Top Book:", s.peek())
-print("Popped Book:", s.pop())
-s.display()
-print("Stack Size:", s.size())
+size_input = int(input("Enter the maximum size of the stack: "))
+s = Stack(size_input)
+
+while True:
+    print("\n--- Stack Operations Menu ---")
+    print("1. Push")
+    print("2. Pop")
+    print("3. Peek")
+    print("4. Display")
+    print("5. Size")
+    print("6. Exit")
+    
+    choice = input("Enter your choice (1-6): ")
+    
+    if choice == "1":
+        book = input("Enter the name of the book to push: ")
+        s.push(book)
+    elif choice == "2":
+        print("Popped Book:", s.pop())
+        
+    elif choice == "3":
+        print("Top Book:", s.peek())
+    elif choice == "4":
+        s.display()
+    elif choice == "5":
+        print("Stack Size:", s.size())
+    elif choice == "6":
+        print("Exiting program.")
+        break
+    else:
+        print("Invalid choice, please select between 1 and 6.")
